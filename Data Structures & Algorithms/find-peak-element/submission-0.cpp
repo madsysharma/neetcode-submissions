5b6f8d283ef88fb5687b2836f9044facf033030f
madsysharma <108534984+madsysharma@users.noreply.github.com>
2026-05-03 19:44:06 +0530

    Add: find-peak-element - submission-0

---
+++ Data Structures & Algorithms/find-peak-element/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size() - 1;
+        while(l <= r)
+        {
+            int m = l + (r - l)/2;
+            if(m > 0 && nums[m] < nums[m-1])
+            {
+                r = m - 1;
+            }
+            else if(m < nums.size() - 1 && nums[m] < nums[m+1])
+            {
+                l = m + 1;
+            }
+            else
+            {
+                return m;
+            }
+        }
+        return -1;
+    }
+};
