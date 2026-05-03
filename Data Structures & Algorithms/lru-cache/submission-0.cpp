263a23dd02c1cb304c15ebb7dba2fe7548cb12ca
madsysharma <108534984+madsysharma@users.noreply.github.com>
2026-05-03 13:20:13 +0530

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.cpp
@@ -0,0 +1,36 @@
+class LRUCache {
+public:
+    unordered_map<int, pair<int, list<int>::iterator>> cache;
+    list<int> order;
+    int capacity;
+
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+    }
+    
+    int get(int key) {
+        if(cache.find(key) == cache.end())
+        {
+            return -1;
+        }
+        order.erase(cache[key].second);
+        order.push_back(key);
+        cache[key].second = --order.end();
+        return cache[key].first;
+    }
+    
+    void put(int key, int value) {
+        if(cache.find(key) != cache.end())
+        {
+            order.erase(cache[key].second);
+        }
+        else if(cache.size() == capacity)
+        {
+            int leastRecent = order.front();
+            order.pop_front();
+            cache.erase(leastRecent);
+        }
+        order.push_back(key);
+        cache[key] = {value, --order.end()};
+    }
+};
